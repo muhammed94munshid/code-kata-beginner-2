9b8696cd30e77e01d720c144fdb504f0dfe33007
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-02-15 11:47:53 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,11 @@
+#a is first term, n is No.of terms,d is diffrerece
+
+a=int(input())
+
+n=int(input())
+
+d=int(input())
+
+s=(a+(n-1)*d)
+
+print("first term",a,"no.of terms",n,"diffrence is",d,"sum ofap is:",s)
