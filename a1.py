78c3ef7eba05466cdd9e259f2dc306910ba2246f
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-03-07 03:37:58 -0800

    Create a1.py

---
+++ a1.py
@@ -0,0 +1,21 @@
+x=input("enter the number between 1 to 10:")
+if (x==0):
+	print("zero")
+if (x==1):
+	print("one")
+if (x==2):
+	print("two")
+if (x==3):
+	print("three")
+if (x==4):
+	print("four")
+if (x==5):
+	print("five")
+if (x==6):
+	print("six")
+if (x==7):
+	print("seven")
+if (x==8):
+	print("eight")
+if (x==9):
+	print("nine")
