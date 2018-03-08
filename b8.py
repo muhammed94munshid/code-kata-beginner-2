76d8778476d0e937745ea3f456a8620e1b0b0550
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-03-08 03:54:31 -0800

    Rename b7.py to b8.py

---
+++ b8.py
@@ -0,0 +1,9 @@
+#solution for cube surface area and volume
+# side value=s
+s=int(input())
+#surface area is sa
+#volume of cube
+v=s**3
+sa=6*v
+print("sa of cube is:",sa)
+print("volume of cube is :",v)
