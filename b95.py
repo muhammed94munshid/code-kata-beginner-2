2af18e3de47011ecac61d6b4fc5296a395df18ae
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-03-08 04:14:33 -0800

    Create b95.py

---
+++ b95.py
@@ -0,0 +1,11 @@
+#program for simple intrest
+# i=intrest
+# p=investment
+# r=rate of intrest
+# t=time duration
+p=float(input("investment:"))
+r=float(input("rate of intrest:"))
+t=float(input("time duration :"))
+r=(r/100)
+i=p*r*t
+print("simpe intrest for",p,"investmentin intrest rate of",r,"in years",t,"is",i)
